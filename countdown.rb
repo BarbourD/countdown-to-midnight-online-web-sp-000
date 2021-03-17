c4ac9532435ca93435818012bad7f34808cfe373
David Barbour <danceguy303@gmail.com>
2021-03-17 19:04:14 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
-#write your code here
 
 def countdown
+countdown = 10
+  while countdown > 0
+        puts "#{countdown} SECOND(S)!"
+    countdown -= 1
+  end
+  puts "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep
+countdown_with_sleep = 5
+  while countdown_with_sleep < 5
+    puts "#{countdown_with_sleep}"
+    countdown_with_sleep -= 1
+        sleep(1.sec)
+ end
+end
+countdown
+countdown_with_sleep
